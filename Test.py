6f775c9787e97e2b441a88bf0544c1fdd4388b52
luigi <luigi.da.silva@hotmail.com>
2024-04-29 16:32:01 +0200

    neu

--- Test.py
+++ Test.py
@@ -1,5 +1,12 @@
 
 print("Hello world!")
-print("Luigi")
-print("")
-
+print("Ben")
+a = 1
+if a == 1:
+    print("Ja")
+else:
+    print("nein")
+name = input("Bitte gebe deinen Namen ein: ")
+Schueler = {"Name": name, "Alter": "20", "Anwesenheit": True}
+print(Schueler["Name"])
+print(Schueler["Alter"])
